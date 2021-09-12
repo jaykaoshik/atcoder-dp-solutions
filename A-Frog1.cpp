1172d32c7bf8c9c3466788474d07c0b49a3212b9
Jay Kaoshik <jaynitin.kaoshik2020@vitstudent.ac.in>
2021-09-12 20:59:28 +0530

    Task A

--- A-Frog1.cpp
+++ A-Frog1.cpp
@@ -16,9 +16,9 @@ void solve()
 	dp[1]=abs(h[0]-h[1]);
 	for(ll i=2;i<n;i++)
 	{
-        ll x=abs(h[i]-h[i-1]);
-        ll y=abs(h[i]-h[i-2]);
-        dp[i]=min(dp[i-1]+x,dp[i-2]+y);
+        	ll x=abs(h[i]-h[i-1]);
+        	ll y=abs(h[i]-h[i-2]);
+        	dp[i]=min(dp[i-1]+x,dp[i-2]+y);
 	}
     	cout<<dp[n-1];
 }
